e153b30a0736a15e37c034a5a7860597922b0db6
Markus Jauernik <jauernik.markus@gmail.com>
2026-04-20 09:00:55 +0000

    Resolve "mDNS"

--- src/dns/multicast_advertiser.rs
+++ src/dns/multicast_advertiser.rs
@@ -2,27 +2,23 @@ use mdns_sd::{ServiceDaemon, ServiceInfo};
 
 const SERVICE_TYPE: &str = "_iris._tcp.local.";
 const INSTANCE_NAME: &str = "iris";
-const IP_ADDRESS: &str = "192.168.1.1";
 const HOSTNAME: &str = "iris.local.";
-const PORT: u16 = 8080;
-const PROPERTIES: [(&str, &str); 5] = [
-    ("path", "/"),
-    ("port", "8080"),
-    ("txtvers", "1"),
-    ("txt", "name=iris"),
-    ("version", "0.0.1"),
-];
-pub fn create_multicast_advertiser() -> Result<ServiceDaemon, Box<dyn std::error::Error>> {
+
+pub fn create_multicast_advertiser(port: u16) -> Result<ServiceDaemon, Box<dyn std::error::Error>> {
     let mdns = ServiceDaemon::new()?;
 
     let iris_service = ServiceInfo::new(
         SERVICE_TYPE,
         INSTANCE_NAME,
-        IP_ADDRESS,
         HOSTNAME,
-        PORT,
-        &PROPERTIES[..],
-    )?;
+        "",
+        port,
+        &[
+            ("name", env!("CARGO_PKG_NAME")),
+            ("version", env!("CARGO_PKG_VERSION")),
+        ][..],
+    )?
+    .enable_addr_auto();
 
     mdns.register(iris_service)?;
 
