f569082e82598410d950510a8a743c9e0fff0a90
loens2 <2-LOENS2@users.noreply.gitlab.loens2.com>
2026-05-08 17:43:05 +0200

    Add llm streaming

--- src/ml_engines/interfaces/llm_interface.rs
+++ src/ml_engines/interfaces/llm_interface.rs
@@ -1,5 +1,7 @@
 use crate::data::ml_engines::SystemPromptType;
 use async_trait::async_trait;
+use std::error::Error;
+use std::sync::mpsc;
 
 #[async_trait]
 pub trait LlmInterface: Send + Sync {
@@ -7,5 +9,12 @@ pub trait LlmInterface: Send + Sync {
         &self,
         prompt: String,
         system_prompt_type: SystemPromptType,
-    ) -> Result<String, Box<dyn std::error::Error + Send + Sync>>;
+    ) -> Result<String, Box<dyn Error + Send + Sync>>;
+
+    async fn generate_text_stream(
+        &self,
+        prompt: String,
+        system_prompt_type: SystemPromptType,
+        output_channel: mpsc::Sender<String>,
+    ) -> Result<(), Box<dyn Error + Send + Sync>>;
 }
--- src/ml_engines/openai/openai_llm_adapter.rs
+++ src/ml_engines/openai/openai_llm_adapter.rs
@@ -2,9 +2,14 @@ use crate::data::config::LlmConfig;
 use crate::data::ml_engines::SystemPromptType;
 use crate::ml_engines::helper_functions::system_prompts::match_system_prompt_type;
 use crate::ml_engines::interfaces::llm_interface::LlmInterface;
+use async_openai::traits::EventType;
+use async_openai::types::responses::ResponseStreamEvent;
 use async_openai::{Client, config::OpenAIConfig, types::responses::CreateResponseArgs};
 use async_trait::async_trait;
+use futures_util::StreamExt;
+use log::{debug, error};
 use std::error::Error;
+use std::sync::mpsc;
 
 pub struct OpenAiLlmAdapter {
     openai_client: Client<OpenAIConfig>,
@@ -35,6 +40,62 @@ impl LlmInterface for OpenAiLlmAdapter {
         };
 
         Ok(output_text)
+    }
+
+    async fn generate_text_stream(
+        &self,
+        prompt: String,
+        system_prompt_type: SystemPromptType,
+        output_channel: mpsc::Sender<String>,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let system_prompt =
+            match_system_prompt_type(system_prompt_type, &self.config.system_prompts);
+
+        let request = CreateResponseArgs::default()
+            .model(self.config.model.clone())
+            .prompt(system_prompt)
+            .input(prompt)
+            .build()?;
+
+        let mut stream = self
+            .openai_client
+            .responses()
+            .create_stream(request)
+            .await?;
+
+        let mut sentence_buffer = String::new();
+
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(response_event) => match &response_event {
+                    ResponseStreamEvent::ResponseOutputTextDelta(delta) => {
+                        let text = &delta.delta;
+
+                        sentence_buffer.push_str(&text);
+
+                        if text.contains(['.', '!', '?']) {
+                            let sentence = sentence_buffer.trim().to_string();
+                            if !sentence.is_empty() {
+                                output_channel.send(sentence)?;
+                                sentence_buffer.clear();
+                            }
+                        }
+                    }
+                    _ => {
+                        debug!("\n{}: skipping\n", response_event.event_type());
+                    }
+                },
+                Err(e) => {
+                    error!("\n{e:#?}");
+                }
+            }
+        }
+
+        if !sentence_buffer.is_empty() {
+            let _ = output_channel.send(sentence_buffer.trim().to_string())?;
+        }
+
+        Ok(())
     }
 }
 
