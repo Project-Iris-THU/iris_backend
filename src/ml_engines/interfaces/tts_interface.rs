9738d9836a4cbecf5c2e1f5e5b9cc1ddd352fdc9
loens2 <2-LOENS2@users.noreply.gitlab.loens2.com>
2026-06-24 17:15:46 +0200

    Add streaming capabilities to the tts interface and implementation
    Signed-off-by: loens2 <2-LOENS2@users.noreply.gitlab.loens2.com>

--- src/ml_engines/interfaces/tts_interface.rs
+++ src/ml_engines/interfaces/tts_interface.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use bytes::Bytes;
+use tokio::sync::mpsc::Sender;
 
 #[async_trait]
 pub trait TtsInterface: Send + Sync {
@@ -7,4 +8,10 @@ pub trait TtsInterface: Send + Sync {
         &self,
         text: String,
     ) -> Result<Bytes, Box<dyn std::error::Error + Send + Sync>>;
+
+    async fn generate_audio_stream(
+        &self,
+        text: String,
+        output_channel: Sender<Bytes>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
 }
--- src/ml_engines/openai/openai_tts_adapter.rs
+++ src/ml_engines/openai/openai_tts_adapter.rs
@@ -2,10 +2,17 @@ use crate::data::config::TtsConfig;
 use crate::ml_engines::interfaces::tts_interface::TtsInterface;
 use async_openai::Client;
 use async_openai::config::OpenAIConfig;
-use async_openai::types::audio::{CreateSpeechRequestArgs, SpeechModel, Voice};
+use async_openai::types::audio::{
+    CreateSpeechRequest, CreateSpeechRequestArgs, CreateSpeechResponseStreamEvent, SpeechModel,
+    Voice,
+};
 use async_trait::async_trait;
+use base64::{Engine as _, engine::general_purpose::STANDARD};
 use bytes::Bytes;
+use futures_util::StreamExt;
+use log::error;
 use std::error::Error;
+use tokio::sync::mpsc::Sender;
 
 pub struct OpenAiTtsAdapter {
     openai_client: Client<OpenAIConfig>,
@@ -15,15 +22,58 @@ pub struct OpenAiTtsAdapter {
 #[async_trait]
 impl TtsInterface for OpenAiTtsAdapter {
     async fn generate_audio(&self, text: String) -> Result<Bytes, Box<dyn Error + Send + Sync>> {
-        let request = CreateSpeechRequestArgs::default()
-            .input(text)
-            .model(SpeechModel::Other(self.config.model.clone()))
-            .voice(Voice::Other(self.config.voice.clone()))
-            .build()?;
+        let request = Self::create_custom_speech_request(
+            text,
+            self.config.model.clone(),
+            self.config.voice.clone(),
+        )?;
 
         let response = self.openai_client.audio().speech().create(request).await?;
 
         Ok(response.bytes)
+    }
+
+    async fn generate_audio_stream(
+        &self,
+        text: String,
+        output_channel: Sender<Bytes>,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let request = Self::create_custom_speech_request(
+            text,
+            self.config.model.clone(),
+            self.config.voice.clone(),
+        )?;
+
+        let mut stream = self
+            .openai_client
+            .audio()
+            .speech()
+            .create_stream(request)
+            .await?;
+
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(response_event) => {
+                    match response_event {
+                        CreateSpeechResponseStreamEvent::SpeechAudioDelta(delta) => {
+                            let audio_base64 = delta.audio;
+
+                            let audio = STANDARD.decode(audio_base64)?;
+
+                            output_channel.send(Bytes::from(audio)).await?;
+                        }
+                        CreateSpeechResponseStreamEvent::SpeechAudioDone(_done) => {
+                            // Do nothing, not useful in this application
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("\n{e:#?}");
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -33,5 +83,17 @@ impl OpenAiTtsAdapter {
             openai_client,
             config,
         }
+    }
+
+    fn create_custom_speech_request(
+        text: String,
+        model: String,
+        voice: String,
+    ) -> Result<CreateSpeechRequest, Box<dyn std::error::Error + Send + Sync>> {
+        Ok(CreateSpeechRequestArgs::default()
+            .input(text)
+            .model(SpeechModel::Other(model))
+            .voice(Voice::Other(voice))
+            .build()?)
     }
 }
